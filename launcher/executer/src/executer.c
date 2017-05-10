d760ca61a46d13226ced882c14f81fe48bff5542
Jaemoon Sim <jettijam@gmail.com>
2017-05-10 16:43:55 +0900

    Added function 'debug' to follow

--- launcher/executer/src/executer.c
+++ launcher/executer/src/executer.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
-
+#include <unistd.h> // getpid, pid_t
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
 void show_usage(char* p){
     printf("(Usage) %s <executable_path>\n", p);
 }
+
+void debug(pid_t pid);
 
 int main(int argc, char** argv){
     if(argc < 2){
         show_usage(argv[0]);
         return 1;
     }
+    
+    char* executable_path = argv[1];
+    
+    pid_t child;
+    child = fork();
+    if(child == 0){ // childe
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        execl(executable_path, executable_path, 0);
+    }else if (child < 0){
+        perror("fork");
+    }else{ // parent
+        debug(child);
+    }
+
     return 1;
+}
+
+void debug(pid_t pid){
+    int status;
+    waitpid(pid, &status, 0);
+    while(WIFSTOPPED(status)){
+        ptrace(PTRACE_CONT, pid, 0, 0);
+        waitpid(pid, &status, 0);
+    }
+    return;
+    //printf("NONONO\n");
 }
