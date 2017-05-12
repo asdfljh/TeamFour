dba55b53ebc9fead79b15a3f2f599d24b2a4d074
Jean CASSOU-MOUNAT <jean.cassou-mounat@insa-lyon.fr>
2017-05-12 16:23:54 +0900

    Initialization of flag_updater.c

---
+++ flagUpdater/flag_updater.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+	return( EXIT_SUCCESS );
+}
+
