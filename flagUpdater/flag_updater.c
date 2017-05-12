4fa73481830801e7acc925a706e50f3eb8b3337d
Jean CASSOU-MOUNAT <jean.cassou-mounat@insa-lyon.fr>
2017-05-12 16:50:27 +0900

    Put client messages into the flag

--- flagUpdater/flag_updater.c
+++ flagUpdater/flag_updater.c
@@ -10,7 +10,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-char* PATH_FLAG = "/var/ctf";
+char* PATH_FLAG = "/var/ctf/";
 int PATH_MAX = 1024;
 int BUFSIZE = 1024;
 
@@ -47,17 +47,82 @@ void daemonize(void)
 
 void listen_client(){
 	FILE *fp;
+	int parentfd; /* parent socket */
+	int childfd; /* child socket */
+	int clientlen; /* byte size of client's address */
+	struct sockaddr_in serveraddr; /* server's addr */
+	struct sockaddr_in clientaddr; /* client addr */
+	struct hostent *hostp; /* client host info */
+	char buf[BUFSIZE]; /* message buffer */
+	char *hostaddrp; /* dotted decimal host addr string */
+	int optval; /* flag value for setsockopt */
+	int n; /* message byte size */
+	int portno = 4200;
 
-	fp = fopen ("notary.flag", "ab+");
+
+	fp = fopen ("notary.flag", "w+");
 	if(fp == NULL){
 		perror("ERROR opening notary.flag");
 	}
 
-	sleep(1);
-	fprintf(fp, "Coucou");
-	fflush(fp);
 
-	fclose(fp);
+	// Create the parent socket 
+	parentfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (parentfd < 0)
+		perror("ERROR opening socket");
+
+	optval = 1;
+	setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
+			(const void *)&optval , sizeof(int));
+
+	// Build the server's Internet address 
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)portno);
+
+	// Associate the parent socket with a port 
+	if (bind(parentfd, (struct sockaddr *) &serveraddr,
+				sizeof(serveraddr)) < 0)
+		perror("ERROR on binding");
+
+	// Make this socket ready to accept connection requests 
+	if (listen(parentfd, 5) < 0) // allow 5 requests to queue up
+		perror("ERROR on listen");
+
+	// Wait for a connection request, echo input line, then close connection 
+	clientlen = sizeof(clientaddr);
+	while (1) {
+
+		// Wait for a connection request 
+		childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+		if (childfd < 0)
+			perror("ERROR on accept");
+
+		// Determine who sent the message 
+		hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+				sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+		if (hostp == NULL)
+			perror("ERROR on gethostbyaddr");
+		hostaddrp = inet_ntoa(clientaddr.sin_addr);
+		if (hostaddrp == NULL)
+			perror("ERROR on inet_ntoa\n");
+		printf("server established connection with %s (%s)\n",
+				hostp->h_name, hostaddrp);
+
+		// Read input string from the client 
+		bzero(buf, BUFSIZE);
+		n = read(childfd, buf, BUFSIZE);
+		if (n < 0)
+			perror("ERROR reading from socket");
+		printf("server received %d bytes: %s", n, buf);
+
+		fprintf(fp, "%s", buf);
+		fflush(fp);
+
+		close(childfd);
+	}
 }
 
 int main(void)
