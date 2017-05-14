d998092a9083795cbc0181bdef0da85ce523efaf
KAISTGUN <signal@kaist.ac.kr>
2017-05-14 22:08:54 +0900

    Set a frame for distributed work

--- notary.py
+++ notary.py
@@ -1,28 +1,34 @@
 import gnupg
 import os
-class notary:
-	def init_server:
-	
-	def import_keys:
-		KEYPATH = './keys/'
-		gpg = gnupg.GPG(gnupghome = KEYPATH)	
-		Keylist = os.listdir(KEYPATH)
-
-		for i in range (len(Keylist)):
-			key_data = open(KEYPATH + Keylist[i]).read()
-			gpg.import_keys(key_data)
-
-	def get_files:			
-
-	def verify(file_data):
-		verified
-		
-	def log_all:
-	
-	def send_sign_with_file:
-	
+from pprint import pprint
+def import_keys():
+	KEYPATH = './keys/'
+	gpg = gnupg.GPG(gnupghome = KEYPATH)	
+	Keylist = os.listdir(KEYPATH)
+	for i in range (len(Keylist)):
+		key_data = open(KEYPATH + Keylist[i]).read()
+		gpg.import_keys(key_data)
+	#pprint (gpg.list_keys())
+def init_server():
+	return
+def get_files():			
+	return
+def verify():		
+	return
+def log_all():
+	return
+def send_sign_with_file():
+	return
 def main():
-	init_server()	
 	import_keys()
-	file = get_files()
-	verify(file)
+	#init_server()	
+	#file = get_files()
+	#verify(file)
+	#log_all()	
+	#send _ sign_ with file
+	#log
+	
+if __name__ == "__main__":
+    main()
+
+	
