68daec64297f7ddc16a8f4d1e55d90bc4a6a0268
Seungwon Woo <seungwonwoo@kaist.ac.kr>
2017-05-15 13:20:12 +0900

    Add json parser to code

--- launcher/src/launcher.c
+++ launcher/src/launcher.c
@@ -4,15 +4,20 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
+#include "jsmn.h"
 
 #define PORT_NUMBER 8001
 #define BUF_SIZE 65536
+#define FILE_NAME_SIZE 256
 
 int port_bind(char* launcher_ip);
 
 uint32_t change_endian(uint32_t src);
 int check_ip_arg(char* start_ip, char* end_ip);
 int check_range(char* start_ip, char* end_ip, char* object_ip);
+
+int jsonParse(char* filePath, char* jsonContents);
+int jsoneq(const char *json, jsmntok_t *tok, const char *s);
 
 int main(int argc, char** argv) {
 	int r;
@@ -21,6 +26,7 @@ int main(int argc, char** argv) {
 	int client_address_size;
 	struct sockaddr_in client_address;
 	char buffer[BUF_SIZE];
+	char filePath[FILE_NAME_SIZE];
 
 	if (argc != 4) {
 		fprintf(stderr, "Usage: %s <launcher_ip> <start_ip> <end_ip>\n", argv[0]);
@@ -66,6 +72,10 @@ int main(int argc, char** argv) {
 			}
 
 			printf("%s, %d\n", buffer, r);
+
+			if (jsonParse(filePath, buffer) == 0) {
+				printf("Client: %s\n", filePath);
+			}
 
 		}
 
@@ -144,3 +154,61 @@ int check_range(char* start_ip, char* end_ip, char* object_ip) {
 
 	return -1;
 }
+
+int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
+			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+		return 0;
+	}
+	return -1;
+}
+
+int jsonParse(char* filePath, char* jsonContents) {
+	int i, r;
+	jsmn_parser p;
+	jsmntok_t t[128];
+	char* nameContents;
+	char* bodyContents;
+	char* fileContents;
+	size_t nameSize = 0;
+	size_t bodySize = 0;
+	size_t fileSize = 0;
+
+	printf("get: %s\n", jsonContents);
+
+	jsmn_init(&p);
+	r = jsmn_parse(&p, jsonContents, strlen(jsonContents), t, sizeof(t)/sizeof(t[0]));
+	if (r < 0) {
+		fprintf(stderr, "Failed to parse JSON: %d\n", r);
+		return -1;
+	}
+
+	if (r < 1 || t[0].type != JSMN_OBJECT) {
+		fprintf(stderr, "Object expected\n");
+		return -1;
+	}
+
+	for (i = 1; i < r; i+=2) {
+		if (jsoneq(jsonContents, &t[i], "name") == 0) {
+			nameSize = t[i+1].end - t[i+1].start + 1;
+			nameContents = (char*)malloc(nameSize*sizeof(char));
+			memcpy(nameContents, jsonContents + t[i+1].start, nameSize - 1);
+			nameContents[nameSize] = '\0';
+			printf("name: %s\n", nameContents);
+		} else if  (jsoneq(jsonContents, &t[i], "body") == 0) {
+			bodySize = t[i+1].end - t[i+1].start + 1;
+			bodyContents = (char*)malloc(bodySize*sizeof(char));
+			memcpy(bodyContents, jsonContents + t[i+1].start, bodySize - 1);
+			bodyContents[bodySize] = '\0';
+			printf("body: %s\n", bodyContents);
+		}
+	}
+
+	fileContents = base64_decode(bodyContents, bodySize - 1, &fileSize);
+
+	free(nameContents);
+	free(bodyContents);
+
+	return 0;
+}
+
