4daf66065911554789bba0211ecb75466f6a287b
Seungwon Woo <seungwonwoo@kaist.ac.kr>
2017-05-15 12:23:08 +0900

    Add port_bind function and basic server program source

--- launcher/src/launcher.c
+++ launcher/src/launcher.c
@@ -11,8 +11,9 @@
 uint32_t change_endian(uint32_t src);
 int check_ip_arg(char* start_ip, char* end_ip);
 
+int port_bind(char* launcher_ip);
 
-void main(int argc, char** argv) {
+int main(int argc, char** argv) {
 	int r;
 	int server_socket;
 	int client_socket;
@@ -30,7 +31,77 @@ void main(int argc, char** argv) {
 		exit(1);
 	}
 
+	server_socket = port_bind(argv[1]);
+
+	if (server_socket == -1) {
+		exit(1);
+	}
+
+	while(1) {
+		client_address_size = sizeof(client_address);
+		client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_size);
+
+		if (client_socket == -1) {
+			fprintf(stderr, "accept() error\n");
+			exit(1);
+		}
+
+		printf("Hello %s\n", inet_ntoa(client_address.sin_addr));
+
+		while(1) {
+			memset(buffer, 0, BUF_SIZE);
+			memset(filePath, 0, FILE_NAME_SIZE);
+
+			r = read(client_socket, buffer, BUF_SIZE);
+
+			if (r <= 0) {
+				break;
+			}
+
+			printf("%s, %d\n", buffer, r);
+
+		}
+
+		close(client_socket);
+
+	}
+	close(server_socket);
+
+	return 0;
+
 }
+
+int port_bind(char* launcher_ip) {
+	int server_socket;
+	struct sockaddr_in server_address;
+
+	server_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+	if (server_socket == -1) {
+		fprintf(stderr, "socket() error\n");
+		return -1;
+	}
+
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(launcher_ip);
+	server_address.sin_port = htons(PORT_NUMBER);
+
+	if (bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
+		fprintf(stderr, "bind() error\n");
+		close(server_socket);
+		return -1;
+	}
+
+	if (listen(server_socket, 5) == -1) {
+		fprintf(stderr, "listen() error\n");
+		close(server_socket);
+		return -1;
+	}
+
+	return server_socket;
+}
+
 
 uint32_t change_endian(uint32_t src) {
 	uint32_t a, b, c, d, result;
@@ -54,4 +125,5 @@ int check_ip_arg(char* start_ip, char* end_ip) {
 
 	return -1;
 }
+
 
