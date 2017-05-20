e43310883c88263217b9bcef8d917282ffdebb2d
Seungwon Woo <seungwonwoo@kaist.ac.kr>
2017-05-20 21:49:26 +0900

    Combine debug function with launcher.c

--- launcher/src/launcher.c
+++ launcher/src/launcher.c
@@ -26,6 +26,7 @@ int jsoneq(const char *json, jsmntok_t *tok, const char *s);
 void makeFile(char* filePath, char* nameContents, size_t fileSize, unsigned char* fileContents);
 
 int verify(char* base64_output);
+void debug(pid_t pid);
 
 int main(int argc, char** argv) {
 	int r;
@@ -297,5 +298,30 @@ int verify(char* base64_output){
 	pclose(fp);
 
 	return 0;
+}
+
+void debug(pid_t pid) {
+	int syscall_num, status;
+	struct user_regs_struct regs;
+	waitpid(pid, &status, 0);
+	ptrace(PTRACE_SYSCALL, pid, 0, 0);
+	while(WIFSTOPPED(status)){
+		ptrace(PTRACE_GETREGS, pid, 0, &regs);
+		syscall_num = regs.orig_eax;
+		if(syscall_num == -1){ // Error occurred
+			printf("Error occurred at runtime\n");
+			kill(pid, 9);
+			return;
+		}
+		if(syscall_num == 11){ // execve
+			printf("SYSCALL #%d called\n", syscall_num);
+			kill(pid, 9);
+			return;
+		}
+		ptrace(PTRACE_SYSCALL, pid, 0, 0);
+		waitpid(pid, &status, 0);
+	}
+	return;
+	//printf("NONONO\n");
 }
 
