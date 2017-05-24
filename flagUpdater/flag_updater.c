dd210df439d0456358a06769b0c2bacc64b6b1d8
Jean Cassou-Mounat <jean.cassou-mounat@insa-lyon.fr>
2017-05-24 21:26:22 +0900

    Delete printf and add comments

--- flagUpdater/flag_updater.c
+++ flagUpdater/flag_updater.c
@@ -17,6 +17,7 @@ int portno = 42;
 
 void daemonize(void)
 {
+  //Make the flag-updater as a daemon
   pid_t pid, sid;
   pid = fork();
 
@@ -35,6 +36,8 @@ void daemonize(void)
     exit( EXIT_FAILURE );
   }
 
+  //Shut stdin, stdout and stderr to show 
+  //no messages
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
   close(STDERR_FILENO);
@@ -51,12 +54,12 @@ int verify_signature(char buf[BUFSIZE]){
 
   // GPG Decryption
   fp = fopen("received_encrypt.flag.gpg", "w+");
-  fprintf(fp, "%s", buf);
+  //fprintf(fp, "%s", buf);
   fclose(fp);
 
   // TODO : to test use received_encrypt.flag.gpg 
   fp = popen("python flagUpdater/decrypt_gpg.py"
-    " > verif.flag", "r"); 
+      " > verif.flag", "r"); 
   fgets(path, BUFSIZE, fp);
   pclose(fp);
 
@@ -81,14 +84,14 @@ int verify_signature(char buf[BUFSIZE]){
         i++;
         if(i == 5){
           strcpy(githubID, token);
-	}
-	if(i == 10){
+        }
+        if(i == 10){
           strcpy(newflag, token);
-	}
-	if(i == 15){
+        }
+        if(i == 15){
           strcpy(signature, token);	
-	}
-	token = strtok(NULL, s);
+        }
+        token = strtok(NULL, s);
       }
     }
   }
@@ -101,58 +104,49 @@ int verify_signature(char buf[BUFSIZE]){
 
   /* Verify the signature */
 
-  char check_signature[BUFSIZE];
-  char *decode64_signature;
-
-  strcpy(check_signature, githubID);
-  strcat(check_signature, ":");
-  strcat(check_signature, newflag);
-
-  puts(check_signature);
-
   //Decode64 the signature
   //encrypt64.flag contains the signature 64-encoded 
   //and signed by one of the TA
   fp = fopen("signature_encrypt64.flag", "w+"); 
-  fprintf(fp, "%s", signature);
+  //fprintf(fp, "%s", signature);
   fclose(fp);
-	
+
   //decode_64.flag.gpg contains the signature 
   //signed by one of the TA
   fp = popen("python flagUpdater/script_64.py " 
-    "> signature_decrypt64.flag.gpg", "r"); 
+      "> signature_decrypt64.flag.gpg", "r"); 
   fgets(path, BUFSIZE, fp);
   pclose(fp);
-	
+
   // Check the signer
   //decode_64.flag.gpg contains the signature 
   //signed by one of the TA
   fp = popen("gpg --list-packets --batch signature_decrypt64.flag.gpg " 
-    "> signer_information.flag 2>&1", "r"); 
+      "> signer_information.flag 2>&1", "r"); 
   fgets(path, BUFSIZE, fp);
   pclose(fp);
 
   if(strcmp(githubID, "DaramG") == 0){
     fp = popen("grep -c \"HyungSeok Han\" signer_information.flag " 
-      "> number.flag", "r"); 
+        "> number.flag", "r"); 
     fgets(path, BUFSIZE, fp);
     pclose(fp);
   }
   else if(strcmp(githubID, "sangkilc") == 0){
     fp = popen("grep -c \"Sang Kil Cha\" signer_information.flag "
-      "> number.flag", "r"); 
+        "> number.flag", "r"); 
     fgets(path, BUFSIZE, fp);
     pclose(fp);
   }
   else if(strcmp(githubID, "soomin-kim") == 0){
     fp = popen("grep -c \"Soomin Kim\" signer_information.flag "
-      "> number.flag", "r"); 
+        "> number.flag", "r"); 
     fgets(path, BUFSIZE, fp);
     pclose(fp);
   }
   else if(strcmp(githubID, "jchoi2022") == 0){
     fp = popen("grep -c \"Jaeseung Choi\" signer_information.flag "
-      "> number.flag", "r"); 
+        "> number.flag", "r"); 
     fgets(path, BUFSIZE, fp);
     pclose(fp);
   }
@@ -183,7 +177,7 @@ void listen_client(){
   int optval; /* flag value for setsockopt */
   int n; /* message byte size */
   char path[BUFSIZE];
-  
+
   // Create the parent socket 
   parentfd = socket(AF_INET, SOCK_STREAM, 0);
   if (parentfd < 0)
@@ -191,7 +185,7 @@ void listen_client(){
 
   optval = 1;
   setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
-    (const void *)&optval , sizeof(int));
+      (const void *)&optval , sizeof(int));
   // Build the server's Internet address 
   bzero((char *) &serveraddr, sizeof(serveraddr));
 
@@ -201,47 +195,47 @@ void listen_client(){
 
   // Associate the parent socket with a port 
   if (bind(parentfd, (struct sockaddr *) &serveraddr,
-    sizeof(serveraddr)) < 0)
-  perror("ERROR on binding");
+        sizeof(serveraddr)) < 0)
+    perror("ERROR on binding");
 
   // Make this socket ready to accept connection requests 
   if (listen(parentfd, 5) < 0) // allow 5 requests to queue up
     perror("ERROR on listen");
 
   // Wait for a connection request, echo input line, then close connection 
-    clientlen = sizeof(clientaddr);
+  clientlen = sizeof(clientaddr);
   while (1) {
 
     // Wait for a connection request 
     childfd = accept(parentfd, 
-      (struct sockaddr *) &clientaddr, &clientlen);
+        (struct sockaddr *) &clientaddr, &clientlen);
     if (childfd < 0)
       perror("ERROR on accept");
 
     // Determine who sent the message 
     hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-      sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        sizeof(clientaddr.sin_addr.s_addr), AF_INET);
     if (hostp == NULL)
       perror("ERROR on gethostbyaddr");
     hostaddrp = inet_ntoa(clientaddr.sin_addr);
     if (hostaddrp == NULL)
       perror("ERROR on inet_ntoa\n");
-    printf("server established connection with %s (%s)\n",
-      hostp->h_name, hostaddrp);
+    //printf("server established connection with %s (%s)\n",
+        hostp->h_name, hostaddrp);
 
     // Read input string from the client 
     bzero(buf, BUFSIZE);
     n = read(childfd, buf, BUFSIZE);
     if (n < 0)
       perror("ERROR reading from socket");
-    printf("server received %d bytes: %s", n, buf);
+    //printf("server received %d bytes: %s", n, buf);
 
     if(verify_signature(buf) == -1){
       perror("The signature of the file isn't good\n");
     }else{
       //Updates the content of the file
       fp = popen("mv verif.flag /var/ctf/; "	
-        "mv /var/ctf/verif.flag /var/ctf/notary.flag", "r");
+          "mv /var/ctf/verif.flag /var/ctf/notary.flag", "r");
       fgets(path, BUFSIZE, fp);
       pclose(fp);
     }
